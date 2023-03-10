1665c4983fbec531adb0778197edc25b526a69ed
Mateo Lohezic <mateolohezic@gmail.com>
2023-03-10 14:02:43 -0300

    DEploy ahora si infeliz

--- routes/codigo.js
+++ routes/codigo.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const route = express.Router();
 const { cargarCodigo } = require('../controllers/codigo');
+const { postData } = require('../controllers/codigoPost');
 
 route.post('/agregar-codigo', cargarCodigo);
+route.post('/post-codigo', postData)
 
 module.exports = route;
